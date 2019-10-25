06326e74873afcf9e05c1294951bcf85daef2ccc
ananthhokage <30351895+ananthhokage@users.noreply.github.com>
2019-10-25 20:41:56 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+
+cascPath1 = "haarcascade_frontalface_default.xml"
+cascPath2 = "haarcascade_profileface.xml"
+facecascade=cv2.CascadeClassifier(cascPath1)
+facecascade2=cv2.CascadeClassifier(cascPath2)
+cap=cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+while(True):
+    ret, frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces2=facecascade2.detectMultiScale(gray,
+            scaleFactor=1.3,
+            minNeighbors=5)
+    faces=facecascade.detectMultiScale(gray,
+            scaleFactor=1.3,
+            minNeighbors=5)
+    for (x,y,w,h) in faces2:
+        col=frame[y:y+h,x:x+w]
+        img="myimage.jpg"
+        cv2.imwrite(img,col)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    for (x,y,w,h) in faces:
+        col=frame[y:y+h,x:x+w]
+        img="myimage.jpg"
+        cv2.imwrite(img,col)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break;
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,34 @@
+
+import cv2
+import sys
+# Get user supplied values
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY )
+
+
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.3,
+    minNeighbors=5,
+    minSize=(30,30)
+
+)
+
+print("Found {0} faces!".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 3)
+cv2.imwrite('tmp2.jpg',image)
+cv2.namedWindow('Faces found', cv2.WINDOW_NORMAL)
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
